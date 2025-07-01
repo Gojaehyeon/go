e39a25d6ac4649299e37bfab52fa75fe2e753c78
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-07-02 00:28:23 +0900

    fix: 이미지 대소문자 수정

---
+++ src/app/page.tsx
@@ -0,0 +1,69 @@
+const imageIcons = [
+  "batterygo.png",
+  "clipgo.png",
+  "displaygo.png",
+  "foldergo.png",
+  "sharego.png",
+  "takeagain.jpg",
+  "dodot.png",
+  "letgo.jpg",
+  "refreshgo.jpg",
+];
+
+const apps = [
+  {
+    name: "BatteryGO",
+    desc: "A simple and beautiful battery status app for Mac. Monitor your battery health at a glance.",
+    url: "https://apps.apple.com/us/app/batterygo/id6747229304?l=en-GB&mt=12",
+    img: "/images/batterygo.png",
+  },
+  {
+    name: "ClipGO",
+    desc: "A smart clipboard manager that helps you organize and quickly access your copied items.",
+    url: "https://apps.apple.com/kr/app/clipgo/idYOUR_APP_ID",
+    img: "/images/clipgo.png",
+  },
+  {
+    name: "DisplayGO",
+    desc: "A display management tool for Mac. Easily control and arrange your external displays.",
+    url: "https://apps.apple.com/kr/app/displaygo/id6746069948?l=en-GB&mt=12",
+    img: "/images/displaygo.png",
+  },
+  {
+    name: "FolderGO",
+    desc: "Organize and manage your folders efficiently on Mac with FolderGO.",
+    url: "https://apps.apple.com/kr/app/foldergo/id6747735485?l=en-GB&mt=12",
+    img: "/images/foldergo.png",
+  },
+  {
+    name: "ShareGO",
+    desc: "A seamless sharing utility to quickly send files and links between your devices.",
+    url: "https://apps.apple.com/kr/app/sharego/idYOUR_APP_ID",
+    img: "/images/sharego.png",
+  },
+  // iOS Apps
+  {
+    name: "Takeagain",
+    desc: "Minimal video camera for quick retakes. Instantly save or retake with a simple UI.",
+    url: "https://apps.apple.com/kr/app/takeagain/id6745813619?l=en-GB",
+    img: "/images/takeagain.jpg",
+  },
+  {
+    name: "Dodot",
+    desc: "Goal-based routine & to-do manager. Build habits and track your progress.",
+    url: "https://apps.apple.com/kr/app/%EB%91%90%EB%8B%B7/id6744737392?l=en-GB",
+    img: "/images/dodot.png",
+  },
+  {
+    name: "LetGo",
+    desc: "Minimal writing app for building a writing habit. Distraction-free and easy to use.",
+    url: "https://apps.apple.com/kr/app/letgo/id6746320131?l=en-GB",
+    img: "/images/letgo.jpg",
+  },
+  {
+    name: "RefreshGO",
+    desc: "Wellness app to refresh your mind and body. Track routines and build healthy habits.",
+    url: "https://apps.apple.com/kr/app/refreshgo/id6745421205?l=en-GB",
+    img: "/images/refreshgo.jpg",
+  },
+]; 
